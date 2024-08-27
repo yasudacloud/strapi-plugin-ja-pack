d775ec0c76a0873d83f3e31019e7a706eb392354
Tetsuya Kikuchi <tkikuchi2000@gmail.com>
2024-08-27 20:49:57 +0900

    fix: paths to json

--- develop/compare.js
+++ develop/compare.js
@@ -1,35 +1,68 @@
 'use strict';
 
+// Set path to packages. ref. https://github.com/strapi/strapi/tree/develop/packages/
+const basePath = `${__dirname}/../../strapi/packages`
 const target = [
   {
-    source: './node_modules/@strapi/admin/admin/src/translations/en.json',
-    dest: './node_modules/@strapi/admin/admin/src/translations/ja.json',
+    source: 'core/admin/admin/src/translations/en.json',
+    dest: 'core/admin/admin/src/translations/ja.json',
     prefix: ''
   },
   {
-    source: './node_modules/@strapi/plugin-content-type-builder/admin/src/translations/en.json',
-    dest: './node_modules/@strapi/plugin-content-type-builder/admin/src/translations/ja.json',
+    source: 'core/content-releases/admin/src/translations/en.json',
+    dest: 'core/content-releases/admin/src/translations/ja.json',
+    prefix: 'content-releases'
+  },
+  {
+    source: 'core/content-type-builder/admin/src/translations/en.json',
+    dest: 'core/content-type-builder/admin/src/translations/ja.json',
     prefix: 'content-type-builder'
   },
   {
-    source: './node_modules/@strapi/plugin-email/admin/src/translations/en.json',
-    dest: './node_modules/@strapi/plugin-email/admin/src/translations/ja.json',
+    source: 'core/email/admin/src/translations/en.json',
+    dest: 'core/email/admin/src/translations/ja.json',
     prefix: 'email'
   },
   {
-    source: './node_modules/@strapi/plugin-i18n/admin/src/translations/en.json',
-    prefix: 'i18n'
-  },
-  {
-    source: './node_modules/@strapi/plugin-upload/admin/src/translations/en.json',
-    dest: './node_modules/@strapi/plugin-upload/admin/src/translations/ja.json',
+    source: 'core/upload/admin/src/translations/en.json',
+    dest: 'core/upload/admin/src/translations/ja.json',
     prefix: 'upload'
   },
   {
-    source: './node_modules/@strapi/plugin-users-permissions/admin/src/translations/en.json',
-    dest: './node_modules/@strapi/plugin-users-permissions/admin/src/translations/ja.json',
+    source: 'plugins/cloud/admin/src/translations/en.json',
+    dest: 'plugins/cloud/admin/src/translations/ja.json',
+    prefix: 'cloud'
+  },
+  {
+    source: 'plugins/color-picker/admin/src/translations/en.json',
+    dest: 'plugins/color-picker/admin/src/translations/ja.json',
+    prefix: 'color-picker'
+  },
+  {
+    source: 'plugins/documentation/admin/src/translations/en.json',
+    dest: 'plugins/documentation/admin/src/translations/ja.json',
+    prefix: 'documentation'
+  },
+  {
+    source: 'plugins/graphql/admin/src/translations/en.json',
+    dest: 'plugins/graphql/admin/src/translations/ja.json',
+    prefix: 'graphql'
+  },
+  {
+    source: 'plugins/i18n/admin/src/translations/en.json',
+    dest: 'plugins/i18n/admin/src/translations/ja.json',
+    prefix: 'i18n'
+  },
+  {
+    source: 'plugins/sentry/admin/src/translations/en.json',
+    dest: 'plugins/sentry/admin/src/translations/ja.json',
+    prefix: 'sentry'
+  },
+  {
+    source: 'plugins/users-permissions/admin/src/translations/en.json',
+    dest: 'plugins/users-permissions/admin/src/translations/ja.json',
     prefix: 'users-permissions'
-  }
+  },
 ]
 
 /**
@@ -39,19 +72,19 @@ const target = [
 async function main() {
   let translations = {};
   for (const lang of target) {
-    const source = (await import(`${__dirname}/../../../../${lang.source}`, {with: {type: "json"}})).default
-    let pluginTranslation;
-    if (!lang.dest) {
-      // Add all of them since they are only in English
-      pluginTranslation = Object.keys(source);
-    } else {
-      // What is in English but not in Japanese
-      const dest = (await import(`${__dirname}/../../../../${lang.dest}`, {with: {type: "json"}})).default
+    const source = (await import(`${basePath}/${lang.source}`, {with: {type: "json"}})).default
+    let dest, pluginTranslation
+    try {
+      dest = (await import(`${basePath}/${lang.dest}`, {with: {type: "json"}})).default
       const sourceKeys = Object.keys(source)
       const destKeys = Object.keys(dest)
       const targetKeys = sourceKeys.filter(sourceKey => !destKeys.some(destKey => sourceKey === destKey))
       pluginTranslation = targetKeys;
+    } catch (error) {
+      // Add all of them since they are only in English
+      pluginTranslation = Object.keys(source);
     }
+
     for (const targetKey of pluginTranslation) {
       const key = lang.prefix ? `${lang.prefix}.${targetKey}` : targetKey
       if (translations[key]) {
